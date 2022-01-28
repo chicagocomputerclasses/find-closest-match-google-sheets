62c7225b7f6c1a90b252e43747ee38fe35347cdc
chicagocomputerclasses <info@chicagocomputerclasses.com>
2022-01-27 18:49:17 -0600

    Create SimilarStringFinder.js

---
+++ v1.1/SimilarStringFinder.js
@@ -0,0 +1,30 @@
+class SimilarStringFinder {
+  constructor(targetPhrase,phraseList, minScore = 0){
+    this.targetPhrase = targetPhrase
+    this.phraseList = phraseList
+    this.scores = null
+    this.minScore = minScore
+    this.bestMatch = {phrase:null, score: null, position: null}
+    this.findScores()
+  }
+
+  findScores(){
+   this.scores = this.phraseList.map((phrase,i) => {
+
+      if(phrase === null || phrase === "" || typeof phrase !== "string") return { phrase:null, score: null, position: i }
+
+      const score = stringSimilarity(this.targetPhrase, phrase)
+
+      const currentMatch = { phrase:phrase, score: score, position: i }
+
+      if(score < this.minScore) return currentMatch
+
+      if(this.bestMatch.score === null || this.bestMatch.score < currentMatch.score){
+        this.bestMatch = {...currentMatch}
+      }
+      return currentMatch
+    })
+
+  }
+
+}
